8bba3e1f908589fa8d2f8e8ae70469f2aa3e5c15
Josh King <actaneon@gmail.com>
2011-02-02 19:13:37 -0800

    Updated gem removal

--- removeOldGems.sh
+++ removeOldGems.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+
+for X in `gem list | grep -v ","` 
+do
+    CMD=`echo $X | tr "~" " " | tr "_" " " | sed -E "s/(.*) \(.*, (.*)\)/\1 -v \2/"`
+    echo "$CMD"
+    gem uninstall $CMD
+done
+
 for X in `gem list | grep "," | tr " " "~"` 
 do
     CMD=`echo $X | tr "~" " " | tr "_" " " | sed -E "s/(.*) \(.*, (.*)\)/\1 -v \2/"`
