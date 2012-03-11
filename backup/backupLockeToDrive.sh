83c2c50f04e34ce6c7d33043c43522dc4e455834
Josh King <actaneon@gmail.com>
2012-03-10 16:05:06 -0800

    Update backup script

--- backup/backupLockeToDrive.sh
+++ backup/backupLockeToDrive.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 tar cjpvf \
-  /Volumes/LACIE/Locke_`date +%Y%m%d`.tbz \
+  /Volumes/LACIE/Sabin_`date +%Y%m%d`.tbz \
   --exclude="*/.Trash*" \
   --exclude="*/.rvm*" \
   --exclude="*/Desktop*" \
   --exclude="*/Downloads*" \
+  --exclude="*/Dropbox*" \
   --exclude="*/Movies*" \
   --exclude="*/Music*" \
   --exclude="*/Pictures*" \
