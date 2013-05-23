13c7098db1a8c7c193b35d0b5dd2354c4fe4368a
Josh King <actaneon@gmail.com>
2013-05-22 17:33:09 -0700

    SSH completion from config

---
+++ ssh-completion.sh
@@ -0,0 +1,15 @@
+_ssh()
+{
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts=`cat ~/.ssh/config | grep ^Host | cut -d' ' -f2-`
+
+  # if [[ ${cur} == -* ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    # return 0
+  # fi
+}
+
+complete -F _ssh ssh josh@
