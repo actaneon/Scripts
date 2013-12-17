ffcda7487e4feb5f0e6dc3d3741e5c1aa728d0aa
Josh King <actaneon@gmail.com>
2013-12-17 10:39:02 -0800

    Git cleanup local remotes

--- git_cleanup_local_remotes.sh
+++ git_cleanup_local_remotes.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git branch -r | grep -vE "(billing|db_cleanup|josh|dev|master|origin\/release)" | xargs git branch -rd
+#git branch -r | grep -vE "origin\/(widget|josh|dev|master|integration|release)" | xargs git branch -rd
+git branch -r | grep -vE "origin\/(josh|feature|gooddata|platform|services|master|integration|release)" | xargs git branch -rd
