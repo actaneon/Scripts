fdf2ee140ea536ae86adb67337324b395fd2c568
Josh King <actaneon@gmail.com>
2015-10-22 11:18:59 -0700

    Update git_reset_branches to only update the main branches that exist locally

--- git_reset_branches.sh
+++ git_reset_branches.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-for X in live master release
-do
-git update-ref refs/heads/$X refs/remotes/origin/$X
+for X in `git branch | grep -E "(master|release|live)" | cut -b3-`; do
+  git update-ref refs/heads/$X refs/remotes/origin/$X
 done
